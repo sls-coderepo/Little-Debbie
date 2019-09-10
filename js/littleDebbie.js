4bd4e034f06bd263f4f579e7883f46180caeb611
unknown <[shrestha_sl@hotmail.com]>
2019-09-09 22:48:25 -0500

    littleDebbie object created

---
+++ js/littleDebbie.js
@@ -0,0 +1,27 @@
+const littleDebbie = [
+
+    {
+       sweetType: "chocolateChip",
+       filling: "chocolate",
+       quantity: 24,
+       cost: "4.75"
+   
+   },
+   {
+       sweetType: "swillRoll",
+       filling: "vanilla",
+       quantity: 12,
+       cost: "5.75"
+   
+   },
+   {
+       sweetType: "devilSquare",
+       filling: "icing",
+       quantity: 12,
+       cost: "2.75"
+   
+         
+   }
+   ]
+   
+   console.log(littleDebbie);
